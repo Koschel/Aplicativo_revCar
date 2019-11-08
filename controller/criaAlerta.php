6ffd26e17d9b29dbf0ca938a44873878eb425b5b
Koschel <50303190+Koschel@users.noreply.github.com>
2019-11-08 20:13:35 -0300

    Ajuste da Aula

--- controller/criaAlerta.php
+++ controller/criaAlerta.php
@@ -1,12 +1,9 @@
-<?
-include("../controller/controleAcesso.php");
-include("../controller/conexao.php");
-
+<?php
 $sqlUm = "select sum(q.km) as totalKm from quilometragem q INNER JOIN carro c on q.id_carro = c.id where q.data_inserido >= c.dt_ultima_revisao";
 $resultUm = mysqli_query($conn, $sqlUm);
 $dadoUm = $resultUm -> fetch_array();
 
-
-
+var_dump($dadoUm);
+die();
 
 ?>
